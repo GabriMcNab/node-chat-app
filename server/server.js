67879051a1376e42993f2ddcd929091997146758
Gabriele Fazio <Gabriele@MBP-di-Gabriele.homenet.telecomitalia.it>
2018-10-17 21:08:05 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,15 +15,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected!');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the Chat App!!!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (newMessage) => {
     console.log('create message', newMessage);
-
     io.emit('newMessage', {
       ...newMessage,
       createdAt: new Date().getTime()
     });
   });
-  
+
   socket.on('disconnect', () => {
     console.log('user disconnected!');
   });
