63703ab548c798c2908e352e4a5b94bfe89f22a1
Gabriele Fazio <Gabriele@MBP-di-Gabriele.homenet.telecomitalia.it>
2018-10-18 09:20:58 +0200

    Format timestamps using moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -9,19 +9,19 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log(message);
-
+  const fromattedTime = moment(message.createdAt).format('h:mm a');
   const li = $('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${fromattedTime}: ${message.text}`);
 
   $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
+  const fromattedTime = moment(message.createdAt).format('h:mm a');
   const li = $('<li></li>');
   const a = $('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${fromattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
 
